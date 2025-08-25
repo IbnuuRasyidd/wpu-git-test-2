5189ce018fd592562b86f63e62771d0af0e22c3e
ibnu  rasyid <haloibnu01@gmail.com>
2025-08-25 11:50:42 +0800

    nambah list

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 <body>
     <h1>helo</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, culpa atque iure ratione sunt a molestiae dolor consequuntur maxime impedit dignissimos voluptas et rem minus inventore fugiat quidem tenetur tempora?</p>
+    <li>odcnwiwc</li>
 </body>
 </html>
