438df1efa5f4d52e72561d6079a1908415dad374
ibnu  rasyid <haloibnu01@gmail.com>
2025-08-25 11:46:39 +0800

    menambah paragraph

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>helo</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, culpa atque iure ratione sunt a molestiae dolor consequuntur maxime impedit dignissimos voluptas et rem minus inventore fugiat quidem tenetur tempora?</p>
 </body>
 </html>
